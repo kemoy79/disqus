f34011e0b87198e62c0cc55b3b468eb1d38fcc45
kemoy79 <kemoy79@gmail.com>
2020-04-26 14:41:40 -0500

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+    validates :first_name, presence :true, length: {minimum:3, maximum:100}
+
+    validates :last_name, presence :true, length: {minimum:3, maximum:100}
+    
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence :true, 
+                      uniqueness: { case_sensitive: false  },
+                      format: { with: EMAIL_REGEX }
 end
