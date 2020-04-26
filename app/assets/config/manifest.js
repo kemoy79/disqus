bf0835fffaec19b45ec0471093f6e062a6db4538
kemoy79 <kemoy79@gmail.com>
2020-04-26 13:39:17 -0500

    Initial Commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link bootstrap.css
+//= link mdb.css
+//= link jquery.js
+//= link bootstrap.js
+//= link mdb.js
+//= link popper.min.js
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  def index
+    @title = 'Home'
+  end
+end
