b733d0125f543ddef31f0732aa78fac985bda10c
kemoy79 <kemoy79@gmail.com>
2020-04-27 23:55:37 -0500

    Added post controller, views and resource routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+
+  def new
+    
+  end
+
+  def show
+   
+  end
+  
+
+
+  def create
+    @object = Object.new(params[:object])
+    if @object.save
+      flash[:success] = "Object successfully created"
+      redirect_to @object
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def edit
+   
+  end
+
+  def update
+    @object = Object.find(params[:id])
+      if @object.update_attributes(params[:object])
+        flash[:success] = "Object was successfully updated"
+        redirect_to @object
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+
+  def def destroy
+    @object = Object.find(params[:id])
+    if @object.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to objects_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to objects_url
+    end
+  end
+  
+  
+  
+  
+
+end
