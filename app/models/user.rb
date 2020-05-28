f13915b4976ddd6e8178d55516440b3dd2eb06e2
kemoy79 <kemoy79@gmail.com>
2020-05-27 20:23:07 -0500

    Added devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,6 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-
-  has_many :posts
-  
-  before_save { self.eamil = eamil.downcase }
-
-  validates :first_name, presence: true, length: { minimum: 3, maximum: 100 }
-
-  validates :last_name, presence: true, length: { minimum: 3, maximum: 100 }
-
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-
-  validates :eamil, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
-
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/migrate/20200426231040_add_password_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddPasswordToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :password_digest, :string, after: 'eamil'
-    # Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-  end
-end
